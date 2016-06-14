d6674e7e92e97bbd6fe1c968b762d93da181484a
Subramanian, Singaram <to.ramsubramanian@gmail.com>
2016-06-13 23:53:01 -0700

    added another unit test for TodoResource.getAllTodos()

--- src/test/java/org/zetrahytes/todoapi/resources/TodoResourceTest.java
+++ src/test/java/org/zetrahytes/todoapi/resources/TodoResourceTest.java
@@ -11,11 +11,16 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.zetrahytes.todoapi.db.TodoDAO;
 import org.zetrahytes.todoapi.entity.Todo;
 
+import com.google.common.collect.ImmutableList;
+
 import io.dropwizard.testing.junit.ResourceTestRule;
+import jersey.repackaged.com.google.common.collect.Lists;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -46,6 +51,17 @@ public class TodoResourceTest {
     @After
     public void tearDown() {
         reset(todoDAO);
+    }
+
+    @Test
+    public void getAllTodos() {
+        final List<Todo> todoList = ImmutableList.of(todo);
+        when(todoDAO.findAllTodos()).thenReturn(todoList);
+        final List<Todo> response = resources.client().target("/todos")
+                .request(MediaType.APPLICATION_JSON)
+                .get(new GenericType<List<Todo>>() {});
+        verify(todoDAO).findAllTodos();
+        assertThat(response).containsAll(todoList);
     }
 
     @Test
